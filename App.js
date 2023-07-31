9425fc68ab25d0b607c5ea0eb0f1b098a5d3981f
ebrardev <16155052@mersin.edu.tr>
2023-07-31 19:04:03 +0300

    expo navigation yapısı

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text,Button,View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import React, { useState } from "react"
 
-export default function App() {
+
+const Profile = ({navigation}) => {
+  const [user,setUser] = useState("ebrar")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View>
+     <Text>You have to SignIn ({user})</Text>
+      <Button title="Sign Up"
+       onPress={() => navigation.navigate("SignUp")} />
+      <Button title="Sign In" onPress={() =>  navigation.navigate("SignIn", {user})} />
+     </View>
+     )
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const SignUp = ({navigation}) => {
+    return (
+      <View>
+        <Text>dont have u account ?</Text>
+        <Button title="Sign In" onPress={() => navigation.navigate("SignIn")} />
+        <Text>Go to proifile</Text>
+        <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
+      </View>
+    )
+    
+    }
+    const SignIn = ({route}) => {
+      
+
+      return(
+        <View>
+          <Text> You sign in    {route.params.user}</Text>
+          <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} />
+        </View>
+      )
+        
+      }
+
+       const Stack = createNativeStackNavigator()
+
+      const App = () => {
+        return <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="SignIn" component={SignIn} 
+          options={({ route }) => ({ title: `Sign in (${route.params.user})` })} />
+
+            </Stack.Navigator>
+
+        </NavigationContainer>
+      }
+
+      export default App;
