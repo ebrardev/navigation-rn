117f032f9ab9e1d25cebc4ac7aedc153509fa41f
ebrardev <16155052@mersin.edu.tr>
2023-08-02 23:29:56 +0300

    navi

--- App.js
+++ App.js
@@ -1,57 +1,72 @@
-import { Text,Button,View } from "react-native"
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import React, { useState } from "react"
+import { Text, Button, View } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-
-const Profile = ({navigation}) => {
-  const [user,setUser] = useState("ebrar")
+const Profile = ({ navigation }) => {
+  const [user, setUser] = useState("ebrar");
   return (
-  <View>
-     <Text>You have to SignIn ({user})</Text>
-      <Button title="Sign Up"
-       onPress={() => navigation.navigate("SignUp")} />
-      <Button title="Sign In" onPress={() =>  navigation.navigate("SignIn", {user})} />
-     </View>
-     )
-  }
+    <View>
+      <Text>You have to SignIn ({user})</Text>
+      <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} />
+      <Button title="Sign In" onPress={() => navigation.navigate("SignIn", { user })} />
+    </View>
+  );
+};
 
-  const SignUp = ({navigation}) => {
-    return (
-      <View>
-        <Text>dont have u account ?</Text>
-        <Button title="Sign In" onPress={() => navigation.navigate("SignIn")} />
-        <Text>Go to proifile</Text>
-        <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
-      </View>
-    )
-    
-    }
-    const SignIn = ({route}) => {
-      
+const SignUp = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Dont have an account?</Text>
+      <Button title="Sign In" onPress={() => navigation.navigate("SignIn")} />
+      <Text>Go to profile</Text>
+      <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
+    </View>
+  );
+};
 
-      return(
-        <View>
-          <Text> You sign in    {route.params.user}</Text>
-          <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} />
-        </View>
-      )
-        
-      }
+const SignIn = ({ route }) => {
+  const navigation = useNavigation(); // useNavigation hook'unu kullanarak navigation değişkenini alıyoruz
 
-       const Stack = createNativeStackNavigator()
+  return (
+    <View>
+      <Text>You signed in as {route.params.user}</Text>
+      <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} />
+    </View>
+  );
+};
 
-      const App = () => {
-        return <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Profile" component={Profile} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="SignIn" component={SignIn} 
-          options={({ route }) => ({ title: `Sign in (${route.params.user})` })} />
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
 
-            </Stack.Navigator>
+const Stack = createNativeStackNavigator();
 
-        </NavigationContainer>
-      }
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+  );
+};
 
-      export default App;
+const Tabs = createBottomTabNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={Home} />
+        <Tabs.Screen name="ProfileStack" component={ProfileStack} />
+      </Tabs.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
